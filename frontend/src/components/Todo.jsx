3b558e958855b5e088fd7c4a948bf9d1ff1c6a0f
Haekal-Mac <haekal.madani@gmail.com>
2025-04-28 21:54:06 +0900

    .

---
+++ frontend/src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div className="todo-container">
+            <div className="todo-header">
+                <h2>To-Do</h2>
+            </div>
+            <div className="todo-content">
+                <p>Your tasks will appear here.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Todo;   
