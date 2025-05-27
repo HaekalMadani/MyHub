e4360229eafb62b68be08c5e41cbe57d07d95d9d
Haekal Damar <haekal.madani@gmail.com>
2025-05-27 23:42:25 +0900

    deploy db part 2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const { checkConnection } = require('./config/database');
 const {default: createAllTable } = require('./utils/dbUtils');
 
-app.listen(PORT, async() => {
+app.listen(async() => {
   console.log(`Backend server running on`);
   try{
     await checkConnection();
